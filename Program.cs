3f62d0dda3fa2807ec61beb1b0811f026c16eef5
Katherine Winters <goble.katherine@gmail.com>
2022-01-18 14:22:28 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+namespace KanyeRest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new QuoteGenerator(client);
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Kayne: {quote.Kayne()}");
+                Console.WriteLine();
+                Console.WriteLine($"Ron: {quote.RonSwanson()}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ QuoteGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace KanyeRest
+{
+    public class QuoteGenerator
+    {
+        private HttpClient client;
+
+        public QuoteGenerator(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public string Kayne()
+        {
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var kanyeReply = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeReply).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonSwanson()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronReply = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronReply);
+
+            return ronQuote[0].ToString();
+
+        }
+    }
+}
